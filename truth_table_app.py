38e66029caef5f1f1438bff01e47ca36a3019728
ChHeYo <cyoo3@u.rochester.edu>
2018-02-21 22:49:44 -0500

    modify gui

--- truth_table_app.py
+++ truth_table_app.py
@@ -1,31 +1,46 @@
-import tkinter as tk
+from tkinter import *
 import tt_entails as tt
 
-window = tk.Tk()
-
-window.title("Truth Table App")
-window.geometry("300x600")
 
 def sat_walk():
     if not knowledge_base.get():
         pass
     else:
         result = tt.walk_sat(str(knowledge_base.get()) + ";")
-        result_display = tk.Text(master=window, height=5, width=30, bg="azure")
-        result_display.grid(column=0, row=4)
-        result_display.insert(tk.END, result)
+        if result:
+            result_display.delete(0, 'end')
+            result_display.insert(0, result)
+        else:
+            result_display.delete(0, 'end')
+            result_display.insert(0, "No model found") 
 
 
-label = tk.Label(text="- : negation \n^ : and \nv : or \n=> : if ... then \n<=> : if and only if", justify="left")
-label.grid(row=0)
+window = Tk()
 
-label1 = tk.Label(text="Please enter knowledge base\n(clauses need to be seperated by commas)")
-label1.grid(row=1)
+window.title("Truth Table App")
+window.geometry("400x400")
+window.resizable(width=False, height=False)
 
-knowledge_base = tk.Entry(bg="azure")
-knowledge_base.grid(row=2)
+frame = Frame(window)
 
-result_sat = tk.Button(text="A model that satisfies given clauses are: ", command=sat_walk)
-result_sat.grid(column=0, row=3)
+strVal = StringVar()
+result = StringVar()
+
+# label = tk.Label(frame, text="- : negation \n^ : and \nv : or \n=> : if ... then \n<=> : if and only if", justify="left")
+# label.grid(row=0)
+
+label1 = Label(frame, text="Please enter knowledge base\n(clauses need to be seperated by commas)")
+label1.grid(row=0, pady=(30, 10))
+
+knowledge_base = Entry(frame, bg="azure", textvariable=strVal)
+knowledge_base.grid(row=1, pady=(10, 10))
+
+result_sat = Button(frame, text="Show", command=sat_walk)
+result_sat.grid(row=2, pady=(10, 40))
+
+result_display = Entry(frame, bg="azure", textvariable=result)
+result_display.grid(row=3)
+
+frame.pack()
 
 window.mainloop()
